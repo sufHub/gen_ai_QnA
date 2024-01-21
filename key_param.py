cbe20879b03bb7eef6005296ce8168e7f9e89813
sufHub <shaikummer@gmail.com>
2024-01-22 01:25:56 +0530

    Update key_param.py

--- key_param.py
+++ key_param.py
@@ -1,2 +1,2 @@
-openai_api_key = "OPEN_API_KEY-pbiPdZWWFc86ByiyhCcwT3BlbkFJ2u0MMM6h4OK1wEsFckFW"
-mongo_uri = "mongodb+srv://USERNAME:XD5G2SoVv7lOiHIO@cluster0.tt76oit.mongodb.net/?retryWrites=true&w=majority"
+openai_api_key = "OPEN_API_KEY"
+mongo_uri = "MONGO_URL"
